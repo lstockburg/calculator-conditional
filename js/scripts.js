96c570f956634d90c906e52a534fc2b01e00f6a5
Brea Borlas and Matt Dowdney <brea@>
2018-03-21 11:15:46 -0700

    Add switch for different button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -18,12 +18,36 @@ var multiply = function(number1, number2) {
 //front-end logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button").click(function(){
+    $("form#calc").submit(event);
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var thisid = this.id
+    var result;
+    switch() {
+      case thisid === "add":
+        result = add(number1, number2);
+        break;
+      case thisid === "subtract":
+        result = subtract(number1, number2);
+        break;
+      case thisid === "multiply":
+        result = multiply(number1, number2);
+        break;
+      case thisid === "divide":
+        result = divide(number1, number2);
+        break;
+    }
     $("#output").text(result);
+
   });
+  // $("form#calc").submit(function(event) {
+  //   event.preventDefault();
+  //   var number1 = parseInt($("#num1").val());
+  //   var number2 = parseInt($("#num2").val());
+  //   var result = add(number1, number2);
+  //
+  //   $("#output").text(result);
+  // });
 });
