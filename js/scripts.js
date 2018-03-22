02827fe347bdc59f951dff25e79a5492566f3bb3
Brea Borlas and Luke Stockburger <brea@>
2018-03-22 15:35:42 -0700

    Change multiple buttons to radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -18,28 +18,27 @@ var multiply = function(number1, number2) {
 //front-end logic
 
 $(document).ready(function() {
-  $("button").click(function(){
-    $("form#calc").submit(event);
-    event.preventDefault();
-    var number1 = parseInt($("#num1").val());
-    var number2 = parseInt($("#num2").val());
-    var thisid = this.id
-    var result;
-    switch(thisid) {
-      case "add":
-        result = add(number1, number2);
-        break;
-      case "subtract":
-        result = subtract(number1, number2);
-        break;
-      case "multiply":
-        result = multiply(number1, number2);
-        break;
-      case "divide":
-        result = divide(number1, number2);
-        break;
-    }
-    $("#output").text(result);
-
+  $("form#calc").submit(function(event){
+  event.preventDefault();
+  var number1 = parseInt($("#num1").val());
+  var number2 = parseInt($("#num2").val());
+  var operation =$("input:radio[name=op-type]:checked").val();
+  var result;
+  switch(operation) {
+    case "add":
+      result = add(number1, number2);
+      console.log(result);
+      break;
+    case "subtract":
+      result = subtract(number1, number2);
+      break;
+    case "multiply":
+      result = multiply(number1, number2);
+      break;
+    case "divide":
+      result = divide(number1, number2);
+      break;
+  }
+  $("#output").text(result);
   });
 });
